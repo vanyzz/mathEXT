115093d3537f65d8cadab2d81b9163b0cf5bbee0
Vanizz <va.tn@bk.ru>
2024-05-25 13:59:25 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_cors import CORS
+import math_formula
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    small_input = request.form.get('small_input')
+    math_formula_result = math_formula.get_math_formula(small_input)
+    return jsonify({'math_formula_result': math_formula_result})
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
